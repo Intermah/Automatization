5ca33e79aeab9936fefd4a822fdd424d39468d38
Intermah <arikilkov@gmail.com>
2022-08-02 11:28:53 +0300

    Still a long way to go, but is's working

--- hellochess.py
+++ hellochess.py
@@ -5,26 +5,32 @@ import os
 from datetime import datetime
 from datetime import timedelta
 
+#Opens Telegram and prints message to the firt person on the list
 def hellochess(): 
     os.system("open -a Telegram")
+    #Time, required to open Telegram    
     time.sleep(4)
     keyboard.press_and_release('Down Arrow')
     keyboard.press_and_release('Enter')
     time.sleep(1)
+    #Message, that you want to be printed
     keyboard.write("Man, LG play some chess")
     keyboard.press_and_release('Enter')
-    time.sleep(4)
-    os.system("pkill Telegram")
 
-#This variable defines the period through what messages will be sent
-period = 1
-curr = time.time()
-const exec_time = curr + timedelta(minutes=period)
+#This variable defines the period between sending messages (in seconds)
+period = 15
+curr = round(time.time())
+#To change the period time unit to minutes, rewrite "seconds=period" to "minutes=period" 
+exec_time = round(curr + timedelta(seconds=period).total_seconds())
 
+#Wait, untill exec_time. than - our function writes a message 
 while True:
     if curr != exec_time:
         time.sleep(1)
-        curr = time.time()
+        curr = round(time.time())
     else:    
         hellochess()
+        curr = round(time.time())
+        #Updating the exec_time to make it run again
+        exec_time = round(curr + timedelta(seconds=period).total_seconds())
     
